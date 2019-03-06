f43b0984e78ad60275f9b63893f07e255962885f
huiminwu <huiminwu24@gmail.com>
2019-03-06 02:04:30 -0500

    Homework is kinda done

--- 07_mongo/mongo.py
+++ 07_mongo/mongo.py
@@ -4,37 +4,58 @@
 #K07 -- Import.Export Bank
 #2019-03-01
 
+'''
+This dataset contained pokemon with
+their various attributes like
+ - num
+ - name
+ - height
+ - weight
+ - type
+ - weakness
+ - spawn chance
+
+Raw data lives here: https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json
+
+Import mechanism:
+ - Remove the beginning '{pokemon' and end '}' so the json file starts and ends with [] respectively
+    - This is because insert_many relies on there to be multiple ids to enumerate and with pokemon originally as the top more attribute, the insert_many will break
+'''
+
 import pymongo
+import json
 SERVER_ADDR = "127.0.0.1"
 connection = pymongo.MongoClient(SERVER_ADDR)
 db = connection.test
 collection = db.jdaughters
 
-#All restaurants in a specified borough
-def find_borough(borough):
-    restaurants = collection.find({"borough":borough})
-    print(list(restaurants))
+with open('pokedex.json', 'r') as f:
+    data = json.load(f)
 
-#All restaurants in a specified zip code
-def find_zip(zipcode):
-    zipcode = str(zipcode)
-    restaurants = collection.find({"address.zipcode":zipcode})
-    print(list(restaurants))
+#inserts multiple id sets of pokemon
+x = collection.insert_many(data)
 
-#All restaurants in a specified zip code and with a specified grade
-def zip_n_grade(zipcode, grade):
-    zipcode = str(zipcode)
-    restaurants = collection.find({"$and":[{"address.zipcode":zipcode}, {"grades.grade":grade}]})
-    print(list(restaurants))
+#for debugging
+#print(x.inserted_ids)
 
-#All restaurants in a specified zip code with a score below a specified threshold
-def zip_n_score(zipcode, score):
-    zipcode = str(zipcode)
-    restaurants = collection.find({"$and":[{"address.zipcode":zipcode}, {"grades.score":{"$lt":score}}]})
-    print(list(restaurants))
+#finds all pokemon with a certain number
+def find_num(num):
+    pokemon = collection.find({"num":num})
+    print(list(pokemon))
+find_num("001")
 
-#FOR TESTING
-#find_borough("Manhattan")
-#find_zip(10001)
-#zip_n_grade(10001, "A")
-#zip_n_score(10001, 5)
+#All pokemon with a specified height and weight
+def h_n_w(height, weight):
+    height = str(height) + " m"
+    weight = str(weight) + " kg"
+    pokemon = collection.find({"$and":[{"height":height}, {"weight":weight}]})
+    print(list(pokemon))
+
+h_n_w(0.99, 13.0)
+
+#All pokemon with a type and a spawn chance less than a number
+def type_n_spawn(type, spawn):
+    pokemon = collection.find({"$and":[{"type":type}, {"spawn":{"$lt":spawn}}]})
+    print(list(pokemon))
+
+type_n_spawn("Grass", 0.05)
