622eb40039695e9110491a49bd365f2755dc9f29
huiminwu <huiminwu24@gmail.com>
2019-03-24 17:34:31 -0400

    Finish k15

--- 15_svg/scatter.js
+++ 15_svg/scatter.js
@@ -1,33 +1,62 @@
-var scale = 25;
-var width = 400;
-var x = d3.scale.linear().range([0, width]);
+// Team VIM: Hui Min Wu and Daniel Keriazis
+// SoftDev2 pd8
+// K15 -- Scattered
+// 2019-03-24
 
-d3.csv("data.csv", type, function(error, data) {
-    x.domain([0, d3.max(data, (d) => d.value)]);
-    var svg = d3.select(".chart")
-                .attr("width", width)
-                .attr("height", 500)
-    svg.selectAll("circle")
-        .data(data)
-        .enter().append("circle")
-        .attr("cx", function(d) {
-            return d.x * 10;
-        })
-        .attr("cy", function(d) {
-            return d.y * 10;
-        })
-        .attr("r", 5)
+var hrs = [3, 8, 7, 3, 2, 1]
+var gpas = [4.0, 0.8, 1.5, 1.6, 2.3, 4.2]
+var data = [[3, 4.0], [8, 0.8], [7, 1.5], [3, 1.6], [2, 2.3], [1, 4.2]]
+var width = 350;
+var height = 350;
+var x = d3.scaleLinear()
+            .domain([d3.min(hrs) - 1, d3.max(hrs) + 3])
+            .range([0, width]);
+var y = d3.scaleLinear()
+            .domain([d3.min(gpas) - 1, d3.max(gpas) + 1])
+            .range([height/2, 0]);
 
-    svg.append("text")
-        .attr("color", "black")
-        .attr("x", 0)
-        .attr("y", 0)
-        .text("hours shlept");
-    }
-);
+var xAxis = d3.axisBottom()
+                    .scale(x);
 
-function type(d) {
-    d.x = +d.x;
-    d.y = +d.y;
-    return d;
-}
+var yAxis = d3.axisLeft()
+                    .scale(y);
+        
+var svg = d3.select(".chart")
+            .attr("width", width)
+            .attr("height", height)
+
+var circles = svg.selectAll("g")
+                .data(data)
+                .enter()
+                .append("circle")
+                .attr("cx", function(d) {
+                    return d[0] * 40 + 20;
+                })
+                .attr("cy", function(d) {
+                    return d[1] * 40;
+                })
+                .attr("r", 5)
+
+svg.append("g")
+    .attr("transform", "translate(45,10)")
+    .call(yAxis);
+
+svg.append("g")
+    .attr("transform", "translate(45,186)")
+    .call(xAxis);
+
+svg.append("text")
+    .attr("x", "-60")
+    .attr("dy", "1em")
+    .attr("y", "5")
+    .attr("transform", "rotate(-90)")
+    .text(" GPAS ");
+
+svg.append("text")
+    .attr("x", "200")
+    .attr("dy", "1em")
+    .attr("y", "220")
+    .text("Hours Slept")
+
+d3.selectAll("text").style("fill", "red");
+
